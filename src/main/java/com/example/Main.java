452bb492b03e137c85e54da27e116130d518c0bb
hlebnoe_pole <glebpavlyuk@gmail.com>
2024-09-26 20:01:57 +0300

    changes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world?");
         System.out.println("this is main branch!!");
+        System.out.println("this is new commit");
     }
 }
